fca71c8cdeccfb7d5d3b2eb421cc4607f66ef4a8
rakhid16 <roploverz@gmail.com>
2020-03-28 14:51:16 +0700

    telah direvisi

--- classifier.py
+++ classifier.py
@@ -1,16 +1,21 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
-# NOTE: Make sure that the class is labeled 'target' in the data file
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1).values
+features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'].values, random_state=None)
+            train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was:1.0
-exported_pipeline = ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.1, min_samples_leaf=12, min_samples_split=19, n_estimators=100)
+# Average CV score on the training set was: 1.0
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=MultinomialNB(alpha=100.0, fit_prior=False)),
+    GaussianNB()
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
--- regressor.py
+++ regressor.py
@@ -1,22 +1,23 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import FeatureAgglomeration
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.linear_model import LassoLarsCV
+from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
+from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
-# NOTE: Make sure that the class is labeled 'target' in the data file
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
-features = tpot_data.drop('target', axis=1).values
+features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'].values, random_state=None)
+            train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was:-3.5543171952759125e-29
+# Average CV score on the training set was: -1.4890733904949598
 exported_pipeline = make_pipeline(
-    VarianceThreshold(threshold=0.05),
-    FeatureAgglomeration(affinity="manhattan", linkage="average"),
-    LassoLarsCV(normalize=True)
+    StackingEstimator(estimator=RandomForestRegressor(bootstrap=True, max_features=0.9000000000000001, min_samples_leaf=1, min_samples_split=2, n_estimators=100)),
+    StackingEstimator(estimator=AdaBoostRegressor(learning_rate=0.001, loss="linear", n_estimators=100)),
+    StackingEstimator(estimator=AdaBoostRegressor(learning_rate=0.5, loss="square", n_estimators=100)),
+    ElasticNetCV(l1_ratio=0.4, tol=0.0001)
 )
 
 exported_pipeline.fit(training_features, training_target)
